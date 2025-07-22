0f6457d46382f2e09e33f8969969f64cab56ef93
UtkarshPrakash19 <143958015+UtkarshPrakash19@users.noreply.github.com>
2025-07-22 15:48:09 +0530

    Update main.tf updated the main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,42 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  description = "Allow HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_instance" "web1" {
-  ami           = "ami-0a1235697f4afa8a4"
-  instance_type = "t2.micro"
+  ami                    = "ami-0a1235697f4afa8a4"
+  instance_type          = "t2.micro"
   associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.lb_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -23,16 +51,13 @@ resource "aws_instance" "web1" {
   tags = {
     Name = "WebServer1"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "aws_instance" "web2" {
-  ami           = "ami-0a1235697f4afa8a4"
-  instance_type = "t2.micro"
+  ami                    = "ami-0a1235697f4afa8a4"
+  instance_type          = "t2.micro"
   associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.lb_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -46,33 +71,24 @@ resource "aws_instance" "web2" {
   tags = {
     Name = "WebServer2"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "aws_lb" "my_lb" {
-  name               = "my-loadbalancer-${random_id.suffix.hex}"
+  name               = "my-loadbalancer"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+  subnets            = data.aws_subnets.default.ids
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "tg-${random_id.suffix.hex}"
+  name     = "tg-simple"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
+    path = "/"
   }
 }
 
@@ -97,71 +113,4 @@ resource "aws_lb_target_group_attachment" "web2_attach" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web2.id
   port             = 80
-}
-
-# VPC, Subnet, and Security Group (minimal setup)
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "MainVPC"
-  }
-}
-
-resource "aws_subnet" "public1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1a"
-}
-
-resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1b"
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.rt.id
-}
-
-resource "aws_route_table_association" "rta2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.rt.id
-}
-
-resource "aws_security_group" "lb_sg" {
-  name        = "lb-sg"
-  description = "Allow HTTP"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
