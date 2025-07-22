94c8e4696e37d00ca702d0b0d3c2857caf4983d8
UtkarshPrakash19 <143958015+UtkarshPrakash19@users.noreply.github.com>
2025-07-22 15:33:41 +0530

    Update main.tf CORRECTED THE GIT LINK

--- main.tf
+++ main.tf
@@ -1,99 +1,154 @@
 provider "aws" {
-  region = "ap-south-1" # Mumbai
+  region = "ap-south-1"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
 resource "aws_instance" "web1" {
   ami           = "ami-0a1235697f4afa8a4"
   instance_type = "t2.micro"
+  associate_public_ip_address = true
+
   user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y git httpd
-              git clone https://github.com/UtkarshPrakash19/StaticSite1.git /var/www/html
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-            EOF
+              yum update -y
+              yum install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              curl -o /var/www/html/index.html https://raw.githubusercontent.com/UtkarshPrakash19/AWSxLBxTERRA/main/webpage101.html
+              EOF
+
   tags = {
-    Name = "web1"
+    Name = "WebServer1"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
 resource "aws_instance" "web2" {
   ami           = "ami-0a1235697f4afa8a4"
   instance_type = "t2.micro"
+  associate_public_ip_address = true
+
   user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y git httpd
-              git clone https://github.com/UtkarshPrakash19/StaticSite2.git /var/www/html
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-            EOF
+              yum update -y
+              yum install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              curl -o /var/www/html/index.html https://raw.githubusercontent.com/UtkarshPrakash19/AWSxLBxTERRA/main/webpage102.html
+              EOF
+
   tags = {
-    Name = "web2"
+    Name = "WebServer2"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_lb" "my_alb" {
-  name               = "my-app-lb"
+resource "aws_lb" "my_lb" {
+  name               = "my-loadbalancer-${random_id.suffix.hex}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
 }
 
-resource "aws_lb_target_group" "my_tg" {
-  name     = "my-target-group"
+resource "aws_lb_target_group" "tg" {
+  name     = "tg-${random_id.suffix.hex}"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.my_alb.arn
+  load_balancer_arn = aws_lb.my_lb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.my_tg.arn
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
 
 resource "aws_lb_target_group_attachment" "web1_attach" {
-  target_group_arn = aws_lb_target_group.my_tg.arn
+  target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web1.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "web2_attach" {
-  target_group_arn = aws_lb_target_group.my_tg.arn
+  target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web2.id
   port             = 80
 }
 
-# Networking Resources:
+# VPC, Subnet, and Security Group (minimal setup)
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MainVPC"
+  }
 }
 
-resource "aws_subnet" "public_1" {
+resource "aws_subnet" "public1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
 }
 
-resource "aws_subnet" "public_2" {
+resource "aws_subnet" "public2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1b"
 }
 
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Allow HTTP inbound traffic"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  description = "Allow HTTP"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -109,8 +164,4 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-output "load_balancer_url" {
-  description = "URL to access your application via ALB"
-  value       = aws_lb.my_alb.dns_name
 }
